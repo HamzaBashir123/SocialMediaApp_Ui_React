74afc0965b42b4208d2c58ec70a5b6f04c25d27b
hamzabashir1268@gmail.com <hamzabashir1268@gmail.com>
2023-10-23 20:55:48 +0500

    complete

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,20 +1,40 @@
-import { useState } from "react";
+import "./profile.css";
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
 
-
-
-const MyComponent = () => {
-  const [count, setCount] = useState(0);
-
-  const handleClick = () => {
-    setCount(prevCount => prevCount + 1);
-  };
-
+export default function Profile() {
   return (
-    <div>
-      <p>Count: {count}</p>
-      <button onClick={handleClick}>Increment</button>
-    </div>
+    <>
+      <Topbar />
+      <div className="profile">
+        <Sidebar />
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img
+                className="profileCoverImg"
+                src="assets/post/3.jpeg"
+                alt=""
+              />
+              <img
+                className="profileUserImg"
+                src="assets/person/7.jpeg"
+                alt=""
+              />
+            </div>
+            <div className="profileInfo">
+                <h4 className="profileInfoName">Safak Kocaoglu</h4>
+                <span className="profileInfoDesc">Hello my friends!</span>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            <Feed />
+            <Rightbar profile/>
+          </div>
+        </div>
+      </div>
+    </>
   );
-};
-
-export default MyComponent;
+}
